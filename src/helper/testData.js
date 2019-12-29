030177c9d78739ca446c6cf52551e299be9b1669
tung1998 <tung16021220@gmail.com>
2019-12-29 10:40:47 +0700

    update

---
+++ src/helper/testData.js
@@ -0,0 +1,93 @@
+export {
+  location3Star,
+  location2Star,
+  location1Star,
+}
+
+
+//Cảnh báo lỗi
+const location3Star = [{
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [105.7827015, 21.0382399]
+    },
+    properties: {
+      title: "Mapbox",
+      imgName: 'ghqg',
+      className: 'marker big',
+      description: "Đại học quốc gia Hà Nội"
+    }
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [105.8827015, 21.0382399]
+    },
+    properties: {
+      title: "Mapbox",
+      imgName: 'ghqg',
+      className: 'marker big',
+      description: "San Francisco, California"
+    }
+  }
+]
+
+
+const location2Star = [{
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [105.7827015, 21.0382399]
+    },
+    properties: {
+      title: "Mapbox",
+      imgName: 'ghqg',
+      className: 'marker medium',
+      description: "Đại học quốc gia Hà Nội"
+    }
+},
+{
+    type: "Feature",
+    geometry: {
+        type: "Point",
+        coordinates: [105.8827015, 21.0382399]
+    },
+    properties: {
+        title: "Mapbox",
+        imgName: 'ghqg',
+        className: 'marker medium',
+        description: "San Francisco, California"
+    }
+  }
+]
+
+
+const location1Star = [{
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [105.7827015, 21.0382399]
+    },
+    properties: {
+      title: "Mapbox",
+      imgName: 'ghqg',
+      className: 'marker small',
+      description: "Đại học quốc gia Hà Nội"
+    }
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [105.8827015, 21.0382399]
+    },
+    properties: {
+      title: "Mapbox",
+      imgName: 'ghqg',
+      className: 'marker small',
+      description: "San Francisco, California"
+    }
+  }
+]

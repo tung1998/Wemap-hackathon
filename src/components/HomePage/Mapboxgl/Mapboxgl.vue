15c6648687d7d5222bce7eb9b4c7d5e9b50779f3
tung1998 <tung16021220@gmail.com>
2019-12-29 09:34:36 +0700

    update

--- src/components/HomePage/Mapboxgl/Mapboxgl.vue
+++ src/components/HomePage/Mapboxgl/Mapboxgl.vue
@@ -12,12 +12,25 @@ function addStudent(data) {
 }
 //cycleHook
 function mounted() {
-  const map = new mapboxgl.Map({
-  container: 'myMap',
-  style: 'https://apis.wemap.asia/vector-tiles/styles/osm-bright/style.json?key=ZpIVSmYKNucNvxlHgRFRVBuj'
+  resizeMap(this)
+  var api =
+    "https://apis.wemap.asia/vector-tiles/styles/osm-bright/style.json?key=ZpIVSmYKNucNvxlHgRFRVBuj";
+  var map = new mapboxgl.Map({
+    container: "myMap",
+    style: api,
+    center: [105.7827015, 21.0382399],
+    zoom: 16
   });
+}
+
+function resizeMap(vue){
+  let windowHeight = window.innerHeight
+  let pageHeaderHeight = $('.page-header').outerHeight()
+  let headerHeight = $('.header').outerHeight()
+  vue.$refs.myMap.style.height = `${windowHeight - pageHeaderHeight - headerHeight}px`
 }
 </script>
 
 <style scoped>
+
 </style>

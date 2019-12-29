ee28e05a1c136bfc72e9f58d731e894ecf569636
Vu Viet Vuong <anhvuonghn99@gmail.com>
2019-12-29 04:32:10 +0000

    mes

--- src/helper/testData.js
+++ src/helper/testData.js
@@ -10,11 +10,11 @@ const location3Star = [{
     type: "Feature",
     geometry: {
       type: "Point",
-      coordinates: [105.7827015, 21.0382399]
+      coordinates: [105.851421,21.029015 ]
     },
     properties: {
-      title: "Mapbox",
-      imgName: 'ghqg',
+      title: "Hồ Gươm",
+      imgName: '../../../assets/img/location/hồ gươm.jpg',
       className: 'marker big',
       description: "Đại học quốc gia Hà Nội"
     }
@@ -23,11 +23,24 @@ const location3Star = [{
     type: "Feature",
     geometry: {
       type: "Point",
-      coordinates: [105.8827015, 21.0382399]
+      coordinates: [ 105.794740,21.037844]
     },
     properties: {
-      title: "Mapbox",
-      imgName: 'ghqg',
+      title: "chùa Hà",
+      imgName: '../../../assets/img/location/250px-ChuaHa_TamQuan.jpg',
+      className: 'marker big',
+      description: "San Francisco, California"
+    }
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [ 105.859584,21.043696 ]
+    },
+    properties: {
+      title: "cầu long biên",
+      imgName: '../../../assets/img/location/cầu long biên.jpg',
       className: 'marker big',
       description: "San Francisco, California"
     }
@@ -39,11 +52,11 @@ const location2Star = [{
     type: "Feature",
     geometry: {
       type: "Point",
-      coordinates: [105.7827015, 21.0382399]
+      coordinates: [105.835255,21.078522]
     },
     properties: {
-      title: "Mapbox",
-      imgName: 'ghqg',
+      title: "bãi đá sông hồng",
+      imgName: '../../../assets/img/location/bãi đá sông hồng.jpg',
       className: 'marker medium',
       description: "Đại học quốc gia Hà Nội"
     }
@@ -52,11 +65,24 @@ const location2Star = [{
     type: "Feature",
     geometry: {
         type: "Point",
-        coordinates: [105.8827015, 21.0382399]
+        coordinates: [105.801010,21.023647]
     },
     properties: {
-        title: "Mapbox",
-        imgName: 'ghqg',
+        title: "tiểu venice",
+        imgName: '../../../assets/img/location/tiểu venice.jpg',
+        className: 'marker medium',
+        description: "San Francisco, California"
+    }
+  },
+  {
+    type: "Feature",
+    geometry: {
+        type: "Point",
+        coordinates: [ 105.851464,21.010022]
+    },
+    properties: {
+        title: "lofita",
+        imgName: '../../../assets/img/location/lofita338 phố huế.jpg',
         className: 'marker medium',
         description: "San Francisco, California"
     }
@@ -68,11 +94,11 @@ const location1Star = [{
     type: "Feature",
     geometry: {
       type: "Point",
-      coordinates: [105.7827015, 21.0382399]
+      coordinates: [105.821017,21.093331]
     },
     properties: {
-      title: "Mapbox",
-      imgName: 'ghqg',
+      title: "cầu nhật tân",
+      imgName: '../../../assets/img/location/cầu nhật tân.jpg',
       className: 'marker small',
       description: "Đại học quốc gia Hà Nội"
     }
@@ -81,11 +107,24 @@ const location1Star = [{
     type: "Feature",
     geometry: {
       type: "Point",
-      coordinates: [105.8827015, 21.0382399]
+      coordinates: [ 105.839266,21.035416]
     },
     properties: {
-      title: "Mapbox",
-      imgName: 'ghqg',
+      title: "đường hoàng diệu",
+      imgName: '../../../assets/img/location/hoàng diệu.jpg',
+      className: 'marker small',
+      description: "San Francisco, California"
+    }
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [ 105.846731, 21.038283]
+    },
+    properties: {
+      title: "phố bích họa phùng hưng",
+      imgName: '../../../assets/img/location/phố bích họa phùng hưng.jpg',
       className: 'marker small',
       description: "San Francisco, California"
     }

9aeecc62cd02f88d8c258d45ac8ed74e7fbbf81f
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-12-29 17:43:22 +0700

    add

--- src/components/HomePage/AddLocation/AddLocation.vue
+++ src/components/HomePage/AddLocation/AddLocation.vue
@@ -3,8 +3,33 @@
 <script>
 export default {
   name: "add-location",
+  mounted
 };
+function mounted() {
+  $(function(){
+  var viewModel = {};
+  viewModel.fileData = ko.observable({
+    dataURL: ko.observable(),
+    // base64String: ko.observable(),
+  });
+  viewModel.multiFileData = ko.observable({
+    dataURLArray: ko.observableArray(),
+  });
+  viewModel.onClear = function(fileData){
+    if(confirm('Are you sure?')){
+      fileData.clear && fileData.clear();
+    }                            
+  };
+  viewModel.debug = function(){
+    window.viewModel = viewModel;
+    console.log(ko.toJSON(viewModel));
+    debugger; 
+  };
+  ko.applyBindings(viewModel);
+});
+}
 </script>
+
 
 <style scoped>
 </style>
